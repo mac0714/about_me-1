022ac791564f3edd990e99a978de43c46ffc544f
mikemiksch <mikemiksch@gmail.com>
2016-10-18 20:21:51 -0700

    Some errors showed up in someone else's linter that didn't show up on mine. I've added missing semicolons to lines 6, 15, 39, and 51. 66 was also cited, but that actually has a semicolon (as do all the lines in the 60s that I can see.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var start = confirm('Hello! Would you like to play a game and see if you know me as well as I know myself? You don\'t, but it never hurts to try now, does it?');
 if (start === true) {
   alert('All right, hold on to your pants! Here comes the first question...');
-  console.log('The visitor has agreed to play the guessing game.')
+  console.log('The visitor has agreed to play the guessing game.');
 } else {
   alert('Well, too bad! You\'re playing whether you want to or not!');
   console.log('The visitor has declined to play the guessing game, but they get to play it anyway.');
@@ -12,7 +12,7 @@ if (start === true) {
 var whereFrom = prompt('Tell me, do you think I am originally from the Seattle area?');
 if (whereFrom.toLowerCase() === 'yes' || whereFrom.toLowerCase() === 'y') {
   alert('Not even close! I\'m from Fresno, California. It is an awful place...');
-  console.log('The visitor incorrectly guessed that I was from Seattle.')
+  console.log('The visitor incorrectly guessed that I was from Seattle.');
 } else if (whereFrom.toLowerCase() === 'no' || whereFrom.toLowerCase() === 'n') {
   alert('Ah, very astute! I\'m from Fresno, California. It is an awful place...');
   console.log('The visitor correctly guessed that I am not from Seattle.');
@@ -36,7 +36,7 @@ if (kids.toLowerCase() === 'yes' || kids.toLowerCase() === 'y') {
   console.log('The visitor thinks I\'m a total breeder with five kids, but I am not.');
 } else if (kids.toLowerCase() === 'no' || kids.toLowerCase() === 'n') {
   alert('Good, because I\'m lying to you. You are quite the savvy truth... determiner... person... thing.');
-  console.log('The visitor correctly guessed that I do not have five kids.')
+  console.log('The visitor correctly guessed that I do not have five kids.');
 } else {
   alert('I\'m sorry, I didn\'t catch that. I only understand yes, no, y, or n!');
   console.log('The visitor entered an unrecognizable answer.');
@@ -48,7 +48,7 @@ if (transformers === true) {
   console.log('The visitor thinks I have way more disposable income than I do and have spent it on little plastic robots for going on 20 years.');
 } else {
   alert('That\'s because I haven\'t. That shit\'s expensive, yo.');
-  console.log('The visitor correctly guessed that I have not collected every Transformer since 1999.')
+  console.log('The visitor correctly guessed that I have not collected every Transformer since 1999.');
 }
 
 var tattoo = prompt('That\'s not to say that I\'m still not a big fan of Transformers, even those terrible, terrible movies that Michael Bay keeps putting out. What if I were to tell you that I have a Decepticon insignia tattooed on my right shoulder, would you believe it?');
